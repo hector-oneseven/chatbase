1b562554da27af5cab220b6154c31ae796c1db46
Hector Ibarra <>
2022-05-08 19:51:13 -0400

    firebase

---
+++ src/utils/firebase.js
@@ -0,0 +1,21 @@
+import firebase from "firebase/app";
+
+const firebaseConfig = {
+
+    apiKey: "AIzaSyBaZPHbpRCRSB-9dYzEuGbv2rJUSvsU-Mc",
+  
+    authDomain: "chat-test-1e607.firebaseapp.com",
+  
+    databaseURL: "https://chat-test-1e607-default-rtdb.firebaseio.com",
+  
+    projectId: "chat-test-1e607",
+  
+    storageBucket: "chat-test-1e607.appspot.com",
+  
+    messagingSenderId: "428842198896",
+  
+    appId: "1:428842198896:web:a5772b641098851e4311b5"
+  
+  };
+
+  const app = initializeApp(firebaseConfig);
